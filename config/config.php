9529a5db3f935b48ee2f66242e4fa38f13104081
Ismael Wismann <wismann.ismael@gmail.com>
2020-12-27 00:11:37 +0100

    Updated initial config file

--- config/config.php
+++ config/config.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-	// bin path to your pdflatex installation
+	// bin path to your pdflatex installation | use 'which pdflatex' on a linux system to find out which is the path to your pdflatex installation
 	'binPath' => 'pdflatex',
 
 	// Folder in your storage folder where you would like to store the temp files created by LaraTeX
-	'tempPath' => '/app/',
+	'tempPath' => 'app/',
 ];
